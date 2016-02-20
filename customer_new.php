a06eddc7d9c1b2091898be6bc3306006456ea069
Akif Quddus <akifquddus@gmail.com>
2016-02-21 00:12:10 +0500

    Issues fixed

--- customer_new.php
+++ customer_new.php
@@ -59,16 +59,16 @@
 					<input type="text" name="CompanyName" class="form-control" required/>
 				</li>
 				<li class="col-sm-6">
+					E-mail address: 
+					<input type="email" name="Email" class="form-control" />
+				</li>
+				<li class="col-sm-6">
 					Contact first name: <span>*</span>
 					<input type="text" name="FirstName" class="form-control" required/>
 				</li>
 				<li class="col-sm-6">
 					Contact last name: <span>*</span>
 					<input type="text" name="LastName" class="form-control" required/>
-				</li>
-				<li class="col-sm-6">
-					E-mail address: 
-					<input type="email" name="Email" class="form-control" />
 				</li>
 				<li class="col-sm-6">
 					Address line 1: 
@@ -78,28 +78,28 @@
 					Address line 2: 
 					<input type="text" name="Address2" class="form-control" />
 				</li>
-				<li class="col-sm-6">
+				<li class="col-sm-4">
 					City: 
 					<input type="text" name="City" class="form-control" />
 				</li>
-				<li class="col-sm-6">
+				<li class="col-sm-4">
 					State: 
 					<select name="State" class="form-control">
 <option value="AL">Alabama</option><option value="AK">Alaska</option><option value="AS">American Samoa</option><option value="AZ">Arizona</option><option value="AR">Arkansas</option><option value="CA">California</option><option value="CO">Colorado</option><option value="CT">Connecticut</option><option value="DE">Delaware</option><option value="DC">District Of Columbia</option><option value="FM">Federated States Of Micronesia</option><option value="FL">Florida</option><option value="GA">Georgia</option><option value="GU">Guam</option><option value="HI">Hawaii</option><option value="ID">Idaho</option><option value="IL">Illinois</option><option value="IN">Indiana</option><option value="IA">Iowa</option><option value="KS">Kansas</option><option value="KY">Kentucky</option><option value="LA">Louisiana</option><option value="ME">Maine</option><option value="MH">Marshall Islands</option><option value="MD">Maryland</option><option value="MA">Massachusetts</option><option value="MI">Michigan</option><option value="MN">Minnesota</option><option value="MS">Mississippi</option><option value="MO">Missouri</option><option value="MT">Montana</option><option value="NE">Nebraska</option><option value="NV">Nevada</option><option value="NH">New Hampshire</option><option value="NJ">New Jersey</option><option value="NM">New Mexico</option><option value="NY">New York</option><option value="NC">North Carolina</option><option value="ND">North Dakota</option><option value="MP">Northern Mariana Islands</option><option value="OH">Ohio</option><option value="OK">Oklahoma</option><option value="OR">Oregon</option><option value="PW">Palau</option><option value="PA">Pennsylvania</option><option value="PR">Puerto Rico</option><option value="RI">Rhode Island</option><option value="SC">South Carolina</option><option value="SD">South Dakota</option><option value="TN">Tennessee</option><option value="TX" selected="">Texas</option><option value="UT">Utah</option><option value="VT">Vermont</option><option value="VI">Virgin Islands</option><option value="VA">Virginia</option><option value="WA">Washington</option><option value="WV">West Virginia</option><option value="WI">Wisconsin</option><option value="WY">Wyoming</option>    </select>
 				</li>
-				<li class="col-sm-6">
+				<li class="col-sm-4">
 					Zip: 
 					<input type="text" name="Zip" class="form-control" />
 				</li>
-				<li class="col-sm-6">
+				<li class="col-sm-4">
 					Phone: <span>*</span>
 					<input type="text" name="Phone" class="form-control" required/>
 				</li>
-				<li class="col-sm-6">
+				<li class="col-sm-4">
 					Alt. Phone:
 					<input type="text" name="AlternatePhone" class="form-control" />
 				</li>
-				<li class="col-sm-6">
+				<li class="col-sm-4">
 					Fax:
 					<input type="text" name="Fax" class="form-control" />
 				</li>
